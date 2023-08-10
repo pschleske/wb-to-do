7127f8c8de1022f2a4e36e040de1e7cb9d5ccb7c
Priscila Schleske <plasch7@gmail.com>
2023-08-09 21:03:23 -0600

    items added twice

---
+++ index.js
@@ -0,0 +1,30 @@
+function addTodo(event) {
+    event.preventDefault();
+    const item = document.createElement("li");
+    item.innerText = document.querySelector("#item").value;
+    item.addEventListener("click", completeTodo)
+
+    const button = document.createElement("button");
+    button.innerText = "x";
+    button.addEventListener("click", removeTodo);
+    item.append(button);
+
+    const list = document.querySelector("ul");
+    list.appendChild(item);
+}
+
+
+function removeTodo(event) {
+    event.target.parentNode.remove();
+}
+
+function completeTodo(event) {
+    const value = event.target.getAttribute(aria - checked);
+    if (value !== "true") {
+        event.target.setAttribute("aria-checked", "true");
+    } else {
+        event.target.setAttribute("aria-checked", "false");
+    }
+}
+
+document.querySelector("form").addEventListener("submit", addTodo);
